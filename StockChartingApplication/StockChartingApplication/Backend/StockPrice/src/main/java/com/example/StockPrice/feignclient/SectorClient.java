0821dec9408abcf83e3b7e555663cfb23cffa607
PonsangeethaJ <95565855+PonsangeethaJ@users.noreply.github.com>
2021-12-12 21:39:29 +0530

    Add files via upload

---
+++ StockChartingApplication/StockChartingApplication/Backend/StockPrice/src/main/java/com/example/StockPrice/feignclient/SectorClient.java
@@ -0,0 +1,18 @@
+package com.example.StockPrice.feignclient;
+
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import  com.example.StockPrice.dto.CompanyDto;
+
+
+@FeignClient("sector-service")
+public interface SectorClient
+{
+    @GetMapping("/sectors/{sectorName}/companies")
+    public List<CompanyDto> getSectorCompanies(@PathVariable String sectorName);
+}
